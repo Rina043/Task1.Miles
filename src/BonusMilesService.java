b9918f5dd44a51fb456e0aeaa6500ef4f44f6f37
Ekaterina Shipkova <katerinashipkova@gmail.com>
2020-07-27 22:05:47 +0400

    Improved task

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,6 @@
 public class BonusMilesService {
-    public int calculate (int cost){ int price = 10000;
-     int miles = price / cost;
-
+    public int calculate (int price) {
+        int miles = price/20;
         return miles;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-
-        int price = 10_000;
-        int miles = service.calculate(20);
+        int miles = service.calculate(100000);
         System.out.println(miles);
     }
 }
